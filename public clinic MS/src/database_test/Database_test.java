af51e47b8f89bb3b369f1ef8685f5f21c8d01179
khaled <khaledhelwane@gmail.com>
2022-12-14 14:10:43 +0200

    Desktop java project

---
+++ public clinic MS/src/database_test/Database_test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database_test;
+
+/**
+ *
+ * @author planet
+ */
+public class Database_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame x=new NewJFrame();
+        x.setVisible(true);
+    }
+    
+}
